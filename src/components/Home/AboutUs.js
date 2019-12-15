63fb2e9ca990b7f1a648f3c4314d0391a6ea4d8a
Radosław Tomaszewski <r.tomaszewski@gmail.com>
2019-12-15 14:13:59 +0100

    Finished AboutUs component

--- src/components/Home/AboutUs.js
+++ src/components/Home/AboutUs.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Decoration from '../../assets/images/Decoration.svg';
+import Signature from '../../assets/images/Signature.svg';
 
-const AboutUs = () => (
-    <h2>AboutUs</h2>
-)
+function AboutUs() {
+    return (
+        <section className="about-us-container">
+            <div className="about-us-text">
+                <h1>O nas</h1>
+                <img src={Decoration}></img>
+                <h2>Lorem ipsum dolor sit amet, consectetur</h2>
+                <h2>adipiscing elit, sed do eiusmod tempor incididunt</h2>
+                <h2>ut labore et dolore magna aliqua.</h2>
+                <h2>Ut enim ad minim</h2>
+                <img style={{ marginTop: "35px", marginRight: "-42%" }} src={Signature}></img>
+            </div>
+            <div className="about-us-image">
+
+            </div>
+        </section>
+    )
+}
+
+
 
 export default AboutUs;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,6 +13,7 @@ class Home extends React.Component {
                 <HomeHeader />
                 <ThreeColumns />
                 <FourSteps />
+                <AboutUs />
             </div>
         )
     }
